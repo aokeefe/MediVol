1856d0a7b92d4de69305cb80ee8d547f929533b1
Mike Lentini <mike@mikelentini.com>
2013-09-13 12:05:11 -0400

    Added Django installation.

--- vagrant-setup/vagrant-setup.sh
+++ vagrant-setup/vagrant-setup.sh
@@ -3,21 +3,21 @@
 echo "Updating apt-get...";
 sudo apt-get update;
 
-echo "Installing vim...";
+printf "\nInstalling vim...";
 sudo apt-get install vim -y;
 
-echo "Installing apache2...";
+printf "\nInstalling apache2...";
 sudo apt-get install apache2 -y;
 
-echo "Installing php5...";
+printf "\nInstalling php5...";
 sudo apt-get install php5 -y;
 
-echo "Installing mysql-server...";
+printf "\nInstalling mysql-server...";
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root';
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root';
 sudo apt-get install mysql-server -y;
 
-echo "Installing phpMyAdmin...";
+printf "\nInstalling phpMyAdmin...";
 echo 'phpmyadmin phpmyadmin/dbconfig-install boolean true' | debconf-set-selections;
 echo 'phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2' | debconf-set-selections;
 
@@ -41,3 +41,9 @@ then
 	echo "Making phpMyAdmin link...";
 	sudo ln -s /usr/share/phpmyadmin /var/www/phpmyadmin;
 fi
+
+printf '\nInstalling python-pip...';
+sudo apt-get install python-pip -y;
+
+printf '\nInstalling Django...';
+sudo pip install Django;
