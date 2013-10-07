ea4a5e624d63e611474e9654979330a12c5c52e0
Bill Beers <nothingatall544@gmail.com>
2013-10-07 13:38:07 -0400

    updated database name

--- MediVol/MediVol/settings.py
+++ MediVol/MediVol/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'inventory',                      # Or path to database file if using sqlite3.
+        'NAME': 'MediVolDB',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'root',
