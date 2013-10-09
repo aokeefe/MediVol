369faeca1bd51adfb1a8cc17d0b38970fe8dead4
Bill Beers <nothingatall544@gmail.com>
2013-10-08 21:38:28 -0400

    removed tables from teh inventory app

--- MediVol/inventory/models.py
+++ MediVol/inventory/models.py
@@ -1,19 +1,5 @@
 from django.db import models
-
-NAME_LENGTH = 128
-
-class Letter(models.Model):
-    letter = models.CharField(max_length=5)
-    name = models.CharField(max_length=NAME_LENGTH)
-
-class Catagory(models.Model):
-	letter = models.ForeignKey(Letter)
-	name = models.CharField(max_length=NAME_LENGTH)
-
-class Item(models.Model):
-	catagory = models.ForeignKey(Catagory)
-	name = models.CharField(max_length=NAME_LENGTH)
-	description = models.CharField(max_length = 500)
+from catalog.models import Item;
 
 class Box(models.Model):
 	destination = models.CharField(max_length=300)
