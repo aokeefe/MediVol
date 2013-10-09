443fde9b8b170122cc090ed75b1bf7f6bc1f6920
Bill Beers <nothingatall544@gmail.com>
2013-10-08 21:39:51 -0400

    Added the catalog app with stuff for it's database

---
+++ MediVol/catalog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+NAME_LENGTH = 128
+
+class Letter(models.Model):
+    letter = models.CharField(max_length=5)
+    name = models.CharField(max_length=NAME_LENGTH)
+
+class Catagory(models.Model):
+	letter = models.ForeignKey(Letter)
+	name = models.CharField(max_length=NAME_LENGTH)
+
+class Item(models.Model):
+	catagory = models.ForeignKey(Catagory)
+	name = models.CharField(max_length=NAME_LENGTH)
+	description = models.CharField(max_length = 500)
