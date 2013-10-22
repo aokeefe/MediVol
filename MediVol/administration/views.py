95d626839052bf865af8c0e5978c95a17d43a404
Shun Mok Bhark <shunmok@me.com>
2013-10-22 16:40:43 -0400

    administration app for login and permission control prototype

---
+++ MediVol/administration/views.py
@@ -0,0 +1,24 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+def not_in_guest_group(user): 
+  if user: 
+    return user.groups.filter(name='guest').count() == 0 
+  return False
+
+@login_required
+@user_passes_test(not_in_guest_group, login_url='/administration/forbidden') 
+def main_page(request):
+  return render_to_response('administration/index.html')
+
+def access_forbidden(request): 
+	return render_to_response('administration/forbidden.html')
+
+#Logout View
+def logout(request):
+  auth.logout(request)
+  return HttpResponseRedirect('/login')
+
+
