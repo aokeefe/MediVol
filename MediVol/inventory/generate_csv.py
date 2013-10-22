f393c44ecbf117c45a47974c58d0e79aea194a0d
Bill Beers <nothingatall544@gmail.com>
2013-10-22 16:43:05 -0400

    spaces

--- MediVol/inventory/generate_csv.py
+++ MediVol/inventory/generate_csv.py
@@ -5,7 +5,7 @@ from django.db import models
 from inventory.models import Box
 
 def generate_csv():
-  time = datetime.datetime.now()
-  csv_file = open(time.strftime("%B_%d_%Y")+".csv", 'w')
-  for box in Box.objects.all():
-    csv_file.write(box.to_csv())
+    time = datetime.datetime.now()
+    csv_file = open(time.strftime("%B_%d_%Y")+".csv", 'w')
+    for box in Box.objects.all():
+        csv_file.write(box.to_csv())
