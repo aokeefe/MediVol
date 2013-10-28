2f9014c6cbfe5ae95ecb9868597e21c276d8a239
Bill Beers <nothingatall544@gmail.com>
2013-10-28 19:40:11 -0400

    fixed ascii dashes and added nullability to inventory model

--- MediVol/inventory/models.py
+++ MediVol/inventory/models.py
@@ -10,26 +10,26 @@ class Box(models.Model):
         (LARGE, 'Large'),
         (UNKNOWN, 'Unknown'),
     )
-    box_id = models.CharField(max_length=4)
-    box_category = models.ForeignKey(Category)
-    box_size = models.CharField(max_length=1, choices=SIZE_CHOICES, default=UNKNOWN)
-    weight = models.DecimalField(max_digits=5, decimal_places=2) 
-    contents = models.CharField(max_length=300)
+    box_id = models.CharField(max_length=4, null=True)
+    box_category = models.ForeignKey(Category, null=True)
+    box_size = models.CharField(max_length=1, choices=SIZE_CHOICES, default=UNKNOWN, null=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=2, null=True) 
+    contents = models.CharField(max_length=300, null=True)
     #zero time is no_exp
     #None is unknown
-    expiration = models.DateTimeField('expiration date')
-    entered_date = models.DateTimeField('date the box was entered')
-    reserved_for = models.CharField(max_length=300)
-    shipped_to = models.CharField(max_length=300)
+    expiration = models.DateTimeField('expiration date', null=True)
+    entered_date = models.DateTimeField('date the box was entered', null=True)
+    reserved_for = models.CharField(max_length=300, null=True)
+    shipped_to = models.CharField(max_length=300, null=True)
     #TODO: Ask Amy what this could mean
-    box_date = models.DateTimeField('Box date')
+    box_date = models.DateTimeField('Box date', null=True)
     #TODO what does this mean?
-    audit = models.IntegerField(default=1)
+    audit = models.IntegerField(default=1, null=True)
     #TODO add the following
     #barcode_value
     #old_box_flag
     #wholesale_value
-    #initials
+    initials = models.CharField(max_length=5, null=True)
     #location = models.CharField(max_length=300)
     def to_csv(self):
         return self.box_id + ", " + self.box_size + ", " + str(self.weight) + ", " + self.contents + ", " + str(self.expiration) + ", " + str(self.entered_date) + ", " + self.reserved_for + ", " + self.shipped_to + ", " + str(self.box_date) + ", " + str(self.audit) + "\n"
