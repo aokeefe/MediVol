3f0e77a19d249f086fec0e39cf2605be137d59f7
Bill Beers <nothingatall544@gmail.com>
2013-10-28 01:35:22 -0400

    added .to_csv

--- MediVol/catalog/models.py
+++ MediVol/catalog/models.py
@@ -6,7 +6,10 @@ class Category(models.Model):
     letter = models.CharField(max_length=1)
     name = models.CharField(max_length=NAME_LENGTH)
     def __unicode__(self):
-        return self.name + " - " + self.letter  
+        return self.name + " - " + self.letter
+
+    def to_csv(self):
+    	return self.letter + ", " + self.name
 
 class BoxName(models.Model):
     letter = models.ForeignKey(Category)
