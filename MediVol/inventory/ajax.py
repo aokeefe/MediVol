7e1400593ca259b548945b3025e1094450bbe2ea
Mike Lentini <mike@mikelentini.com>
2013-11-04 13:44:52 -0500

    Added comments to everything and some more box info.

--- MediVol/inventory/ajax.py
+++ MediVol/inventory/ajax.py
@@ -4,8 +4,11 @@ from datetime import datetime
 from random import randint
 
 from catalog.models import Category, BoxName, Item
-from inventory.models import Box, Box_Contents
+from inventory.models import Box, Contents
 
+"""
+Gets all of the box names associated with a given category.
+"""
 @dajaxice_register(method='GET')
 def get_box_names(request, category_name):
     category = Category.objects.get(name=category_name)
@@ -17,7 +20,10 @@ def get_box_names(request, category_name):
         box_names_array.append(box_name.name)
     
     return simplejson.dumps(box_names_array)
-    
+
+"""
+Gets all of the items associated with a given box name.
+"""
 @dajaxice_register(method='GET')
 def get_items(request, box_name):
     box_name = BoxName.objects.get(name=box_name)
@@ -29,7 +35,11 @@ def get_items(request, box_name):
         items_array.append(item.name)
     
     return simplejson.dumps(items_array)
-    
+
+"""
+Searches categories, box names, and items for a query. 
+Returns the results in 'Category > Box Name > Item' form.
+"""
 @dajaxice_register(method='GET')
 def get_search_results(request, query):
     results_array = []
@@ -49,9 +59,18 @@ def get_search_results(request, query):
     
     return simplejson.dumps(results_array)
 
+"""
+Creates a box with the given initials, weight, size, items, and note.
+
+Item array should be of the form:
+[ [item_name, item_expiration, item_quantity], [item_name, item_expiration, item_quantity] ]
+
+That is, there is an array with arrays inside it that describe the items.
+"""
 @dajaxice_register
 def create_box(request, initials, weight, size, items, note=''):
-    # TODO: how do we generate a box ID?
+    # TODO: generate a real box ID
+    # TODO: store note in box
     box_id = randint(100, 999)
 
     new_box = Box(box_id=box_id, box_size=size[:1], weight=weight, entered_date=datetime.today(), initials=initials)
@@ -59,7 +78,16 @@ def create_box(request, initials, weight, size, items, note=''):
     new_box.save()
     
     for item in items:
-        contents = Box_Contents(box_within=new_box, item=Item.objects.get(name=item[0]), quantity=item[2])
+        if item[1] == 'Never':
+            contents = Contents(box_within=new_box, 
+                item=Item.objects.get(name=item[0]), 
+                quantity=item[2])
+        else:
+            contents = Contents(box_within=new_box, 
+                item=Item.objects.get(name=item[0]), 
+                quantity=item[2], 
+                expiration=item[1])
+        
         contents.save()
     
     return True
