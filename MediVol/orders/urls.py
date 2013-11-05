00f4e2bc50212bccf9f1c3df77ea76435ebdbdcf
Shun Mok Bhark <shunmok@me.com>
2013-11-04 19:02:56 -0500

    trying to again, changed to four spaces..

--- MediVol/orders/urls.py
+++ MediVol/orders/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 from orders.views import *
 
 urlpatterns = patterns ('', 
-	
-	#Orders Main Page 
-	url(r'^$', orders_main), 
+    #Orders Main Page 
+    url(r'^$', orders_main), 
 )
