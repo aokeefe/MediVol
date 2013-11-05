5fdd80f7cd46b969629f121c509986daf2ebca56
Shun Mok Bhark <shunmok@me.com>
2013-11-04 21:53:19 -0500

    made some changes

--- MediVol/notifications/views.py
+++ MediVol/notifications/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+
+#Display main page of notifications
+def notifications_main(request): 
+    return render_to_response('notifications/index.html')
+
