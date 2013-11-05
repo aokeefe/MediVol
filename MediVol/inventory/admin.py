f941f5d99a3592858fca560b0ec0a2da0426f4ed
Bill Beers <nothingatall544@gmail.com>
2013-11-04 19:06:05 -0500

    spacessssss

--- MediVol/inventory/admin.py
+++ MediVol/inventory/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from inventory.models import Box
 
 class BoxAdmin(admin.ModelAdmin):
-  list_display = ('box_id', 'box_size', 'weight', 'old_contents', 'expiration')
-  def get_id(self, obj):
-  	return '%s'%(obj.box.box_id)
-  get_id.short_discription = 'Box Id'
+    list_display = ('box_id', 'box_size', 'weight', 'old_contents', 'expiration')
+    def get_id(self, obj):
+        return '%s'%(obj.box.box_id)
+    get_id.short_discription = 'Box Id'
 
 admin.site.register(Box, BoxAdmin)
