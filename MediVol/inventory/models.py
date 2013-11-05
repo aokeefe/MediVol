4080799e823898438f7b81cb8d8c9dc0d1d760d1
Bill Beers <nothingatall544@gmail.com>
2013-11-05 15:18:26 -0500

    made changes so that the database can be easily exported

--- MediVol/inventory/models.py
+++ MediVol/inventory/models.py
@@ -12,33 +12,39 @@ class Box(models.Model):
         (LARGE, 'Large'),
         (UNKNOWN, 'Unknown'),
     )
-    box_id = models.CharField(max_length=4, null=True)
     box_category = models.ForeignKey(Category, null=True)
+    box_id = models.CharField(max_length=4, null=True)
+
     box_size = models.CharField(max_length=1, choices=SIZE_CHOICES, default=UNKNOWN, null=True)
     weight = models.DecimalField(max_digits=5, decimal_places=2, null=True) 
-    old_contents = models.CharField(max_length=300, null=True)
+    initials = models.CharField(max_length=5, default="")
+    entered_date = models.DateTimeField('date the box was entered', null=True)
+    #location = models.CharField(max_length=300)
+    
     #None is no expiration
     #TODO remove
     old_expiration = models.DateTimeField('expiration date', null=True)
-    entered_date = models.DateTimeField('date the box was entered', null=True)
-    #TODO: remove
-    reserved_for = models.CharField(max_length=300, null=True)
+    old_contents = models.CharField(max_length=300, null=True)
     shipped_to = models.CharField(max_length=300, null=True)
+    reserved_for = models.CharField(max_length=300, null=True)
+
     #TODO: Ask Amy what this could mean
     box_date = models.DateTimeField('Box date', null=True)
     #TODO what does this mean?
     audit = models.IntegerField(default=1, null=True)
+    
     #TODO add the following
     #barcode_value
     #old_box_flag
     #wholesale_value
-    initials = models.CharField(max_length=5, null=True)
-    #location = models.CharField(max_length=300)
+    
     def to_csv(self):
         """
         Returns a string containing all the CSV information of the Box.  Used in creating database backups
         """
-        return self.box_id + ", " + self.box_size + ", " + str(self.weight) + ", " + self.contents + ", " + str(self.expiration) + ", " + str(self.entered_date) + ", " + self.reserved_for + ", " + self.shipped_to + ", " + str(self.box_date) + ", " + str(self.audit) + "\n"
+        return self.box_category.letter + ", " + self.box_id + ", " + self.box_size + ", " + str(self.weight) + ", " +\
+        self.initials + ", " + str(self.entered_date) + ", " + str(self.old_expiration) + ", " + self.old_contents + \
+        ", " + str(self.box_date) + ", " + str(self.audit) + ", " + self.shipped_to + ", " + self.reserved_for
 
     def __unicode__(self):
         """
@@ -49,7 +55,8 @@ class Box(models.Model):
     def get_expiration(self):
         """
         Finds the oldest date amoung the contents of a Box, and return it.
-        For example if an item is expireing on 01-01-2014 and another is expireing on 01-01-2012, 01-01-2012 will be returned
+        For example if an item is expireing on 01-01-2014 and another is expireing on 01-01-2012, 01-01-2012 will be 
+        returned
         """
         NOT_EXPIRING_IN_THIS_MILLENIUM = datetime(3013,1,1,0,0,0,0,pytz.UTC)
         expiration = NOT_EXPIRING_IN_THIS_MILLENIUM
