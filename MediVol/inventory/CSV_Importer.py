d4bad6e993cbdcfd4fa17bf32a26ec190ecc4027
Bill Beers <nothingatall544@gmail.com>
2013-11-05 15:18:26 -0500

    made changes so that the database can be easily exported

--- MediVol/inventory/CSV_Importer.py
+++ MediVol/inventory/CSV_Importer.py
@@ -31,9 +31,11 @@ def importer(filepath):
         if (rownum != 1):
             validatedRow = validate_import_row(row, rownum)
             if (validatedRow != None):  
-                box = Box(box_id=validatedRow[0], box_size=validatedRow[1], weight=validatedRow[2], old_contents=validatedRow[3],
-                    old_expiration=validatedRow[4], entered_date=validatedRow[5], reserved_for=validatedRow[6], shipped_to=validatedRow[7],
-                    box_date=validatedRow[8], audit=validatedRow[9], box_category=Category.objects.filter(letter=validatedRow[10])[0])
+                box = Box(box_id=validatedRow[0], box_size=validatedRow[1], weight=validatedRow[2], 
+                    old_contents=validatedRow[3], old_expiration=validatedRow[4], entered_date=validatedRow[5], 
+                    reserved_for=validatedRow[6], shipped_to=validatedRow[7], box_date=validatedRow[8], 
+                    audit=validatedRow[9], box_category=Category.objects.filter(letter=validatedRow[10])[0], 
+                    initials='old')
                 box.save() 
         rownum += 1
 
