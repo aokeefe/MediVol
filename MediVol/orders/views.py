9f84606df1c886dcdde821bff3fcbbfa3d82ec44
Shun Mok Bhark <shunmok@me.com>
2013-11-04 19:18:17 -0500

    trying again

--- MediVol/orders/views.py
+++ MediVol/orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response
 
 #Display the main page of ordering 
-def orders_main(request): 
-	return render_to_response('orders/index.html')
+def orders_main(request):
+    return render_to_response('orders/index.html')
 
