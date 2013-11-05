1f9c3b01b82706bb9d9db0fab857e8d01cb8d8a0
Bill Beers <nothingatall544@gmail.com>
2013-11-05 15:24:43 -0500

    added box name

--- MediVol/generate_csv.py
+++ MediVol/generate_csv.py
@@ -2,14 +2,15 @@ import sys, os, datetime
 sys.path.append('/var/www/MediVol/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediVol.settings")
 from django.db import models
-from catalog.models import Category, BoxName
+from catalog.models import Category, BoxName, Item
 from inventory.models import Box
 
 def generate_csv():
     print("start")
     time = datetime.datetime.now()
     csv_file = open(time.strftime("%B_%d_%Y")+".csv", 'w')
-    models = [Category, BoxName, Box]
+    #TODO: might want to make this an import to keep it the same across importer and exporter
+    models = [Category, BoxName, Item, Box]
     for model in models:
         for node in model.objects.all():
             csv_file.write(node.to_csv() +"\n")
