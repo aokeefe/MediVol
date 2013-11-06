06b002a91a71076b81eb9a8c315a547cc85bde96
Bill Beers <nothingatall544@gmail.com>
2013-11-05 22:43:12 -0500

    Now with 200000000000000000100000000000000000000000000000000% more tests

--- MediVol/catalog/models.py
+++ MediVol/catalog/models.py
@@ -6,13 +6,6 @@ NAME_LENGTH = 128
 class Category(models.Model):
     letter = models.CharField(max_length=1)
     name = models.CharField(max_length=NAME_LENGTH)
-    
-    @classmethod
-    def from_csv(cls, csv):
-        values = csv.split("&&&")
-        category = Category(letter=str(values[0]), name=values[1])
-        category.save()
-        return category
 
     def __unicode__(self):
         return self.name + " - " + self.letter
@@ -25,20 +18,14 @@ class Category(models.Model):
 
     @classmethod
     def from_csv(cls, csv):
-        values = csv.split(", ")
+        values = csv.split("&&&")
         category = Category(letter=str(values[0]), name=values[1])
         category.save()
+        return category
 
 class BoxName(models.Model):
     category = models.ForeignKey(Category)
     name = models.CharField(max_length=NAME_LENGTH)
-
-    @classmethod
-    def from_csv(cls, csv):
-        values = csv.split("&&&")
-        box_name = BoxName(category=Category.objects.filter(letter=values[0])[0], name=values[1])
-        box_name.save()
-        return box_name
 
     def __unicode__(self):
         return self.name
@@ -48,18 +35,18 @@ class BoxName(models.Model):
 
     def to_csv(self):
         return self.category.letter + "&&&" + self.name
+
+    @classmethod
+    def from_csv(cls, csv):
+        values = csv.split("&&&")
+        box_name = BoxName(category=Category.objects.get(letter=values[0]), name=values[1])
+        box_name.save()
         
 #TODO update to a multi Catagory implementation
 class Item(models.Model):
     box_name = models.ForeignKey(BoxName)
     name = models.CharField(max_length=NAME_LENGTH)
     description = models.CharField(max_length = 500)
-
-    @classmethod
-    def from_csv(cls, csv):
-        values = csv.split("&&&")
-        item = Item(name=values[0], description=values[2], box_name=BoxName.objects.get(name=values[1]))
-        item.save()
 
     def __unicode__(self):
         return self.name
@@ -69,3 +56,9 @@ class Item(models.Model):
 
     def to_csv(self):
         return self.name + "&&&" + self.box_name.name + "&&&" + self.description
+
+    @classmethod
+    def from_csv(cls, csv):
+        values = csv.split("&&&")
+        item = Item(name=values[0], description=values[2], box_name=BoxName.objects.get(name=values[1]))
+        item.save()
