682ff634b78112652c94b116a9b9a0c68672a0e7
Bill Beers <nothingatall544@gmail.com>
2013-11-06 17:14:13 -0500

    import works for catalog

---
+++ MediVol/import_from_csv.py
@@ -0,0 +1,22 @@
+import sys, os, datetime
+sys.path.append('/var/www/MediVol/')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediVol.settings")
+from django.db import models
+from catalog.models import Category, BoxName, Item
+from inventory.models import Box, Contents
+
+def import_csv():
+    #replace with parameter
+    time = datetime.datetime.now()
+    csv_file = open(time.strftime("%B_%d_%Y")+".csv", 'r')
+    #TODO move outside import\export
+    models = [Category, BoxName, Item]#, Box, Contents]
+    csv = csv_file.read()
+    models_csv = csv.split("-----\n")
+    for x in range(0, len(models) -1):
+        splitlines = models_csv[x].splitlines()
+        for y in splitlines:
+            print(y)
+            models[x].from_csv(y)
+
+import_csv()
