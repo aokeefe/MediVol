c5a16c4dd7aab775bd649286e991b4bb48118e31
Bill Beers <nothingatall544@gmail.com>
2013-11-05 23:08:10 -0500

    filled in missing methods

--- MediVol/catalog/models.py
+++ MediVol/catalog/models.py
@@ -6,15 +6,7 @@ NAME_LENGTH = 128
 class Category(models.Model):
     letter = models.CharField(max_length=1)
     name = models.CharField(max_length=NAME_LENGTH)
-    def __unicode__(self):
-        return self.name + " - " + self.letter
-
-    def to_csv(self):
-        return self.letter + "&&&" + self.name
-
-    def __eq__(self, other):
-        return self.letter == other.letter and self.name == other.name
-
+    
     @classmethod
     def from_csv(cls, csv):
         values = csv.split("&&&")
@@ -22,34 +14,51 @@ class Category(models.Model):
         category.save()
         return category
 
+    def __unicode__(self):
+        return self.name + " - " + self.letter
+
+    def __eq__(self, other):
+        return self.letter == other.letter and self.name == other.name
+
+    def to_csv(self):
+        return self.letter + "&&&" + self.name
+
 class BoxName(models.Model):
     category = models.ForeignKey(Category)
     name = models.CharField(max_length=NAME_LENGTH)
-    def __unicode__(self):
-        return self.name
-
-    def to_csv(self):
-        return self.category.letter + "&&&" + self.name
 
     @classmethod
     def from_csv(cls, csv):
         values = csv.split("&&&")
         box_name = BoxName(category=Category.objects.get(letter=values[0]), name=values[1])
         box_name.save()
+
+    def __unicode__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.category == other.category and self.name == other.name
+
+    def to_csv(self):
+        return self.category.letter + "&&&" + self.name
         
-#TODO remove in favor of a multi Catagory implementation
+#TODO update to a multi Catagory implementation
 class Item(models.Model):
     box_name = models.ForeignKey(BoxName)
     name = models.CharField(max_length=NAME_LENGTH)
     description = models.CharField(max_length = 500)
-    def __unicode__(self):
-        return self.name
-
-    def to_csv(self):
-        return self.name + "&&&" + self.box_name.name + "&&&" + self.description
 
     @classmethod
     def from_csv(cls, csv):
         values = csv.split("&&&")
         item = Item(name=values[0], description=values[2], box_name=BoxName.objects.get(name=values[1]))
         item.save()
+
+    def __unicode__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.box_name == other.box_name and self.name == other.name and self.description == other.description
+
+    def to_csv(self):
+        return self.name + "&&&" + self.box_name.name + "&&&" + self.description
