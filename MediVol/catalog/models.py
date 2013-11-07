ac440418780af87d8b41c924e10cf97f6310ed4c
Bill Beers <nothingatall544@gmail.com>
2013-11-07 10:25:57 -0500

    changes per Mike

--- MediVol/catalog/models.py
+++ MediVol/catalog/models.py
@@ -7,6 +7,13 @@ class Category(models.Model):
     letter = models.CharField(max_length=1)
     name = models.CharField(max_length=NAME_LENGTH)
 
+    @classmethod
+    def create_from_csv(cls, csv):
+        values = csv.split("&&&")
+        category = Category(letter=str(values[0]), name=values[1])
+        category.save()
+        return category
+
     def __unicode__(self):
         return self.name + " - " + self.letter
 
@@ -16,19 +23,12 @@ class Category(models.Model):
     def to_csv(self):
         return self.letter + "&&&" + self.name
 
-    @classmethod
-    def from_csv(cls, csv):
-        values = csv.split("&&&")
-        category = Category(letter=str(values[0]), name=values[1])
-        category.save()
-        return category
-
 class BoxName(models.Model):
     category = models.ForeignKey(Category)
     name = models.CharField(max_length=NAME_LENGTH)
 
     @classmethod
-    def from_csv(cls, csv):
+    def create_from_csv(cls, csv):
         values = csv.split("&&&")
         box_name = BoxName(category=Category.objects.filter(letter=values[0])[0], name=values[1])
         box_name.save()
@@ -49,6 +49,12 @@ class Item(models.Model):
     name = models.CharField(max_length=NAME_LENGTH)
     description = models.CharField(max_length = 500)
 
+    @classmethod
+    def create_from_csv(cls, csv):
+        values = csv.split("&&&")
+        item = Item(name=values[0], description=values[2], box_name=BoxName.objects.get(name=values[1]))
+        item.save()
+
     def __unicode__(self):
         return self.name
 
@@ -57,9 +63,3 @@ class Item(models.Model):
 
     def to_csv(self):
         return self.name + "&&&" + self.box_name.name + "&&&" + self.description
-
-    @classmethod
-    def from_csv(cls, csv):
-        values = csv.split("&&&")
-        item = Item(name=values[0], description=values[2], box_name=BoxName.objects.get(name=values[1]))
-        item.save()
