cda04d1026b8be2534ddea9a2c0b5609163b1020
Shun Mok Bhark <shunmok@me.com>
2013-11-10 11:56:15 -0500

    made changes according to CR

--- MediVol/notifications/notifier.py
+++ MediVol/notifications/notifier.py
@@ -1,73 +1,52 @@
 import mandrill
-import uuid
-import datetime
-from notification import Notification
+import os 
+import sys
 
-#All emails come from the system so setting the email address here.
-#NOTE: The email below will need to be changed to InterVols production email once deployed.
+#Executing from DJango shell kept complaining about indentation error so I just 
+#did it this way. The path is relative to whats on the vagrant VM. 
+sys.path.append('/var/www/MediVol')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediVol.settings")
+
+from notifications.models import Notification
+
+#All emails comes from the system so setting the email address here.
+#Note: The email below will need to be changed to Intervols production email once deployed
 sender_email = 'sxb5828@rit.edu'
 
-#TODO: Need to complete this
-#Method to log notification to DB 
-#Stores each notification that comes in in the DB
-def log_notification(subject, message, recipient_email, recipient_name):
+#This method communicates with Mandrill via their API to send out emails
+def send_message(notification):
 
-    id = uuid.uuid1()
-      
-#TODO: 
-#Fetches message from Mandrill using the 
-#tag attribute to search for the specific email.
-def fetch_message(tag):
-    pass 
-    
-#This method communicates to Mandrill via their API to send out emails  
-def send_message(notification): 
-    
     #The API key will need to be changed to InterVols own account. Currently it is using my API key
     api_key = 'x9AjbnyiNKtL0Avv1N7oCw'
+
     subject = notification.subject
     recipient_email = notification.recipient_email
     recipient_name = notification.recipient_name
-    email_id = notification.id 
+    email_id = notification.id
     message = notification.message
-    
+
     try:
         mandrill_client = mandrill.Mandrill(api_key)
-        message = {'from_email': sender_email,
-            'from_name': 'MediVol Test User',
-            'global_merge_vars': [{'content': 'merge1 content', 'name': 'merge1'}],
-            'headers': {'Reply-To': sender_email},
-            'html': message,
-            'important': False,
-            'inline_css': None,
-            'subject': subject,
-            'to': [{'email': recipient_email,
-                    'name': recipient_name,
-                    'type': 'to'}],
-            'track_clicks': None,
-            'track_opens': None,
-            'tracking_domain': None,
-            'url_strip_qs': None,
-            'view_content_link': None,
-                'tags':[email_id]}
-            
-        result = mandrill_client.messages.send(message=message, async=False, ip_pool='Main Pool')
-        print('Resposne from Mandrill API')        
-        print (result)
-           
+        message = {'from_email': sender_email, 'from_name': 'MediVol Test User', 'global_merge_vars': [{'content': 'merge1 content', 'name': 'merge1'}], 'headers': {'Reply-To': sender_email}, 'html': message, 'important': False, 'inline_css': None, 'subject': subject, 'to': [{'email': recipient_email, 'name': recipient_name, 'type': 'to'}], 'track_clicks': None, 'track_opens': None, 'tracking_domain': None, 'url_strip_qs': None, 'view_content_link': None, 'tags': [email_id]}
+
+        response = mandrill_client.messages.send(message=message, async=False, ip_pool='Main Pool')
+        print('Response from Mandrill API')
+        print(response)
+
     except mandrill.Error, e:
         print 'Mandrill Error Occurred: %s - %s' % (e.__class__, e)
-        raise 
+        raise
 
-#This method can be removed next PR as it is only for testing 
-def main(): 
+#This method can be removed next PR as it is only for testing
+def main():
+
     subject_input = raw_input('Enter Email Subject: ')
     email_message = raw_input('Enter Email Message: ')
     recipient_email2 = raw_input('Enter Recipient Email: ')
     recipient_name2 = raw_input('Enter Recipient Name: ')
-    
-    notifier = Notification(email_message, subject_input, recipient_email2, recipient_name2)
+
+    notifier = Notification.create(email_message, subject_input, recipient_email2, recipient_name2)
     send_message(notifier)
-	                           
+
 if __name__ == '__main__':
-        main()
+    main()
