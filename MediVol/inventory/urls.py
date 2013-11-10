55cb14923350a7d61cfe7ccb437f374193d9e12e
Shun Mok Bhark <shunmok@me.com>
2013-11-10 17:31:22 -0500

    added route to boxinfo page

--- MediVol/inventory/urls.py
+++ MediVol/inventory/urls.py
@@ -4,4 +4,5 @@ from inventory import views
 
 urlpatterns = patterns('',
     url(r'^create/$', views.create, name='create'),
+    url(r'^view_box_info/(?P<boxid>\w+)/$', views.box_info, name='box_info'),
 )
