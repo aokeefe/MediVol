6b844b16daf7ea2c841a88536f53e4bc013f8557
Shun Mok Bhark <shunmok@me.com>
2013-11-12 16:44:50 -0500

    removed duplicate create method

--- MediVol/notifications/models.py
+++ MediVol/notifications/models.py
@@ -2,19 +2,10 @@ import uuid
 from django.db import models
 
 class Notification(models.Model): 
-        
-    notification_id = models.CharField(max_length=50) 
+ 
     sent_date = models.DateTimeField(auto_now_add=True)
     message = models.CharField(max_length=2000) 
     subject = models.CharField(max_length=200)
     recipient_email = models.CharField(max_length=100)
-    recipient_name = models.CharField(max_length=50) 
-
-    @classmethod 
-    def create(cls, email_message, email_subject, recipient_mail, recipient_name1):
-        
-        id = str(uuid.uuid4())
-        notification = cls(notification_id=id, message=email_message, subject=email_subject, recipient_email=recipient_mail, recipient_name=recipient_name1)
-
-        return notification
-          
+    recipient_name = models.CharField(max_length=50)
+           
