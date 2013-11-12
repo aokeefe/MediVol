58363c67230e566e486459b6eb50b1da5eb3dbf5
Shun Mok Bhark <shunmok@me.com>
2013-11-12 16:43:10 -0500

    unit test

--- MediVol/notifications/tests.py
+++ MediVol/notifications/tests.py
@@ -4,13 +4,18 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
 from django.test import TestCase
+from notifications.models import Notification
+import notifications.notifier as send_api
 
+class SimpleTestSend(TestCase):
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_basic_send(self):
+    
+        test_message = 'test message' 
+        test_subject = 'test subject' 
+        test_recipient_email = 'sxb5828@rit.edu'
+        test_recipient_name = 'shun'
+        notifier = Notification(message = test_message, subject = test_subject, recipient_email=test_recipient_email, recipient_name=test_recipient_name) 
+ 
+        response = send_api.send_message(notifier)
