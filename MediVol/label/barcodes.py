30a467ba1160231c43472be386aba4371d76d964
Bill Beers <nothingatall544@gmail.com>
2013-11-12 15:35:50 -0500

    started to generate labels

--- MediVol/label/barcodes.py
+++ MediVol/label/barcodes.py
@@ -1,14 +1,45 @@
+import os, sys
+sys.path.append('/var/www/MediVol/')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MediVol.settings")
 from base64 import b64encode
 from reportlab.graphics.barcode import createBarcodeDrawing
 from reportlab.graphics.shapes import Drawing, String
 from reportlab.graphics import renderPM
+from inventory.models import Box
+
+class BoxLabel(Drawing):
+    def __init__(self, barcode_value, width=200, height=320, fontSize=30):
+        barcode = BarcodeGen(barcode_value)
+        barcode.translate((width-barcode.width)/2, 0)
+
+        Drawing.__init__(self, width, height)
+        self.add(barcode, name='barcode')
+
+        box = Box.objects.get(barcode=barcode_value)
+        loc = String(10, 300, "Box Id:")
+        loc.fontSize = 20
+
+        box_id = String(15, 250, box.box_id)
+        box_id.fontSize = 60
+
+        category = String(12, 235, box.box_category.name)
+        category.fontSize = 15
+
+        self.add(loc, "loc")
+        self.add(box_id, "boxId")
+        self.add(category, "category")
+
+    def save_img(self, fileName, Dir='.'):
+        self.save(formats=['png'], fnRoot=fileName)
 
 """
-Used to get code128 type barcodes, text_vale is barcode string, width, height, and fontSize defalt to values that work with printer
+Used to get code128 type barcodes, text_vale is barcode string, width, height, and fontSize defalt to values that work 
+with printer
 """
 class BarcodeGen(Drawing):
-    def __init__(self, text_value, width=2.2, height=80, fontSize=30):
-        barcode = createBarcodeDrawing('Code128', quiet=False, value=text_value, barWidth=width, barHeight=height, fontSize=fontSize, humanReadable=True)
+    def __init__(self, text_value, width=2.2, height=60, fontSize=15):
+        barcode = createBarcodeDrawing('Code128', quiet=False, value=text_value, barWidth=width, barHeight=height, 
+                                       fontSize=fontSize, humanReadable=True)
         Drawing.__init__(self,barcode.width,barcode.height)
         self.add(barcode, name='barcode')
 
@@ -24,3 +55,7 @@ class BarcodeGen(Drawing):
     """
     def save_img(self, fileName, Dir='.'):
         self.save(formats=['png'], fnRoot=fileName)
+
+box = BoxLabel("07019673")
+box = BoxLabel("22338799")
+box.save_img("test.png")
