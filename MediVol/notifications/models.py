9db22578ed5b851d8971ad73814ded8a543d63a3
Shun Mok Bhark <shunny@shuns-air.wireless.rit.edu>
2014-02-25 17:29:57 -0500

    changes from commit

--- MediVol/notifications/models.py
+++ MediVol/notifications/models.py
@@ -1,11 +1,1 @@
-import uuid
-from django.db import models
-
-class Notification(models.Model): 
- 
-    sent_date = models.DateTimeField(auto_now_add=True)
-    message = models.CharField(max_length=2000) 
-    subject = models.CharField(max_length=200)
-    recipient_email = models.CharField(max_length=100)
-    recipient_name = models.CharField(max_length=50)
-           
+          
