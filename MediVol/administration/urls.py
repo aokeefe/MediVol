f1ff059c451d348b34f9e8b4a65404573b50ad07
Mike Lentini <mike@mikelentini.com>
2014-02-25 00:44:57 -0500

    Spacing.

--- MediVol/administration/urls.py
+++ MediVol/administration/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import patterns, url
 from administration.views import *
 
 urlpatterns = patterns ('',
-  #Administration Main Page
-  url(r'^$', main_page),
-
-  #Administration Forbidden Page 
-  url(r'^forbidden/', access_forbidden),
-  
-  url(r'^users/', manage_users), 
-  url(r'^backups/', manage_backups), 
-  url(r'^warehouses/', manage_warehouses), 
+    #Administration Main Page
+    url(r'^$', main_page),
+    
+    #Administration Forbidden Page 
+    url(r'^forbidden/', access_forbidden),
+    
+    url(r'^users/', manage_users), 
+    url(r'^backups/', manage_backups), 
+    url(r'^warehouses/', manage_warehouses)
 )
