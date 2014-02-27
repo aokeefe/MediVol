cffe10066e9ffa01728e8cda3dba257f9b6ea212
Shun Mok Bhark <shunny@shuns-air.wireless.rit.edu>
2014-02-26 21:18:23 -0500

    removed view method as it is unncessary

--- MediVol/notifications/views.py
+++ MediVol/notifications/views.py
@@ -1,6 +1,2 @@
-from django.shortcuts import render_to_response
 
-#Display main page of notifications
-def notifications_main(request): 
-    return render_to_response('notifications/index.html')
 
