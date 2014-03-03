e7c6d76cd8a5b63cc72e944bfd4ee21c05631e05
Bill Beers <nothingatall544@gmail.com>
2014-03-03 11:22:17 -0500

    stuff

--- MediVol/orders/models.py
+++ MediVol/orders/models.py
@@ -4,6 +4,7 @@ from import_export import to_csv
 
 class Order(models.Model):
     #This may need revisiting as a more detailed model becomes available
+    order_id = models.IntegerField(unique=True)
     reserved_for = models.CharField(max_length=30, null=True)
     paid_for = models.BooleanField(default=False)
     shipped = models.BooleanField(default=False)
@@ -15,12 +16,13 @@ class Order(models.Model):
         return self.order_number
 
     def to_csv(self):
-        values = [self.reserved_for,
+        values = [self.order_id,
+                  self.reserved_for,
                   self.paid_for,
                   self.shipped,
                   self.ship_to,
                   self.order_number,
-                  creation_date]
+                  self.creation_date]
         return to_csv_from_array(values)
 
 class OrderBox(models.Model):
@@ -32,4 +34,7 @@ class OrderBox(models.Model):
         return self.box + " in order " + self.order_for
 
     def to_csv(self):
-        return self.order_for.order_number + ", " + self.box.box_id + ","
+        values = [self.order_for.order_id,
+                  self.box.box_id,
+                  self.cost]
+        return to_csv_from_array(values)
