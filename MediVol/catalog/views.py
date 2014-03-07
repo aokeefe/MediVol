55dd2ae6a2fc2965ea360071116e720dbeec09cd
Mike Lentini <mike@mikelentini.com>
2014-03-07 18:30:32 -0500

    Showing boxes with item, and foundation for orders with item. Needs styling.

--- MediVol/catalog/views.py
+++ MediVol/catalog/views.py
@@ -2,9 +2,9 @@ from django.views.generic.list import ListView
 from django.utils import timezone
 from django.shortcuts import render
 
-from catalog.models import Category
-from catalog.models import BoxName
-from catalog.models import Item
+from catalog.models import Category, BoxName, Item
+from inventory.models import Box, Contents
+from orders.models import Order, OrderBox
 
 class ItemsListView(ListView):
 
@@ -21,6 +21,25 @@ def item_info(request, item_id):
     except Item.DoesNotExist:
         item = False
 
-    context = { 'item': item }
+    boxes_with_item = False
+    orders_with_item = False
+
+    if item:
+        contents = Contents.objects.filter(item=item)
+
+        if len(contents) > 0:
+            boxes_with_item = []
+            for content in contents:
+                boxes_with_item.append(content.box_within)
+
+        for box in boxes_with_item:
+            order_boxes = OrderBox.objects.filter(box=box)
+
+            if len(order_boxes) > 0:
+                orders_with_item = []
+                for order_box in order_boxes:
+                    orders_with_item.append(order_box.order_for)
+
+    context = { 'item': item, 'boxes_with_item': boxes_with_item, 'orders_with_item': orders_with_item }
 
     return render(request, 'catalog/item_info.html', context)
