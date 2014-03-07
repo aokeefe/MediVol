d3420e163117f7f4174ba6a87fdef20705b211b0
Bill Beers <nothingatall544@gmail.com>
2014-03-07 09:42:49 -0500

    i/e for orders

--- MediVol/orders/models.py
+++ MediVol/orders/models.py
@@ -12,6 +12,27 @@ class Customer(models.Model):
 
     def __unicode__(self):
         return "contact info for: " + business_name
+
+    def to_csv(self):
+        values = [self.contact_id,
+                  self.contact_name,
+                  self.contact_email,
+                  self.business_name,
+                  self.business_address,
+                  self.shipping_address]
+        return to_csv_from_array(values)
+
+    @classmethod
+    def create_from_csv(cls, csv):
+        filtered_values = to_array_from_csv(csv)
+        customer = Customer(contact_id=filtered_values[0],
+                            contact_name=filtered_values[1],
+                            contact_email=filtered_values[2],
+                            business_name=filtered_values[3],
+                            business_address=filtered_values[4],
+                            shipping_address=filtered_values[5])
+        customer.save()
+        return customer
         
 class Order(models.Model):
     CREATED = 'C'
@@ -28,8 +49,6 @@ class Order(models.Model):
     order_id = models.IntegerField(unique=True)
     reserved_for = models.ForeignKey(Customer, null=True)
     paid_for = models.BooleanField(default=False)
-    shipped = models.BooleanField(default=False)
-    ship_to = models.CharField(max_length=300, null=True)
     order_number = models.IntegerField()
     creation_date = models.DateTimeField('Date the order was made')
     order_status = models.CharField(max_length=1, choices=ORDER_STATUS, default=CREATED)
@@ -41,11 +60,22 @@ class Order(models.Model):
         values = [self.order_id,
                   self.reserved_for.customer_id,
                   self.paid_for,
-                  self.shipped,
-                  self.ship_to,
                   self.order_number,
-                  self.creation_date]
+                  self.creation_date,
+                  self.order_status]
         return to_csv_from_array(values)
+
+    @classmethod
+    def create_from_csv(cls, csv):
+        filtered_values = to_array_from_csv(csv)
+        order = Order(order_id=filtered_values[0],
+                      reserved_for=Customer.objects.get(customer_id=filtered_values[1]),
+                      paid_for=filtered_values[2],
+                      order_number=filtered_values[3],
+                      creation_date=filtered_values[4],
+                      order_status=filtered_values[5])
+        order.save()
+        return order
 
     def get_weight(self):
         order_weight = 0.0
@@ -66,3 +96,12 @@ class OrderBox(models.Model):
                   self.box.box_id,
                   self.cost]
         return to_csv_from_array(values)
+
+    @classmethod
+    def create_from_csv(cls, csv):
+        filtered_values = to_array_from_csv(csv)
+        order_box = OrderBox(order_for=Order.objects,get(order_id=filtered_values[0]),
+                             box=Box.objects.get(box_id=filtered_values[1]),
+                             cost=filtered_values[2])
+        order_box.save()
+        return order_box
