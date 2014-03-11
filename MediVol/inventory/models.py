47920701e69ff58f53ae765045aa58d891e36e57
Bill Beers <nothingatall544@gmail.com>
2014-03-10 20:51:32 -0400

    moved warehouse model to inventory

--- MediVol/inventory/models.py
+++ MediVol/inventory/models.py
@@ -165,3 +165,16 @@ class Contents(models.Model):
                   self.quantity,
                   self.expiration]
         return to_csv_from_array(values)
+
+NAME_LENGTH = 80
+ABBREV_LENGTH = 4
+ADDRESS_LENGTH = 200
+
+
+class Warehouse(models.Model):
+    name = models.CharField(max_length=NAME_LENGTH)
+    abbreviation = models.CharField(max_length=ABBREV_LENGTH)
+    address = models.CharField(max_length=ADDRESS_LENGTH)
+
+    def __unicode__(self):
+        return self.name
