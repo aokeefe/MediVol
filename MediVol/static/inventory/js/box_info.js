5fb5fb81390d9ae5d23fe665224931ef0ebc29ab
ajk8409 <Tony@Anthonys-MacBook-Pro-3.local>
2014-03-17 15:46:45 -0400

    added print button for box info page

---
+++ MediVol/static/inventory/js/box_info.js
@@ -0,0 +1,24 @@
+
+/**
+* Callback for get_label AJAX call.
+*/
+function print(response){
+	if (response != 'False') {
+        var iframe = document.createElement('iframe');
+        var html = '<head><script type"text/javascript">window.print();</script></head>' +
+                '<body>' + response + '</body>';
+        iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+        iframe.width = 0;
+        iframe.height = 0;
+        document.body.appendChild(iframe);
+        
+        setTimeout(function() {
+            location.reload();
+        }, 1);
+    }
+}
+
+
+function print_label(box_id){
+	Dajaxice.inventory.get_label(print, {"box_id" : box_id});
+}
