f8b7aac5ca5dbfed523506ac0fb63fbd2790aa14
= <=>
2014-04-24 13:43:09 -0400

    changes per Mike

--- MediVol/orders/models.py
+++ MediVol/orders/models.py
@@ -59,15 +59,15 @@ class Order(models.Model):
     UNPAID = 'U'
     PAID = 'P'
     SHIPPED = 'S'
-    PAID_DEPOSITE = 'D'
+    PAID_DEPOSIT = 'D'
     CANCELLED = 'F'
     ORDER_STATUS = (
         (CREATED, 'Created'),
         (UNPAID, 'Unpaid For'),
         (PAID, 'Paid For'),
         (SHIPPED, 'Shipped Out'),
-        (PAID_DEPOSITE, 'Deposite Paid'),
-        (CANCELLED, 'Cancelled Order'),
+        (PAID_DEPOSIT, 'Deposite Paid'),
+        (CANCELLED, 'Cancelled'),
     )
     order_number = models.IntegerField(unique=True)
     reserved_for = models.ForeignKey(Customer, null=True)
