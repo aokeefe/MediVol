0890d0cd0fa41f65689e5213e06f4db77a54980d
= <=>
2014-05-20 15:56:47 -0400

    new scrapper is working items 519 boxes fully imported 285

--- MediVol/inventory/new_imports/new_initializer.py
+++ MediVol/inventory/new_imports/new_initializer.py
@@ -7,8 +7,8 @@ import pytz
 from datetime import datetime
 import re
 
-from catalog.models import Category, BoxName
-from inventory.models import Box
+from catalog.models import Category, BoxName, Item
+from inventory.models import Box, Contents
 
 ERROR_COUNT = 0
 UNREAD_LINE_COUNT = 0
@@ -26,8 +26,10 @@ def get_category(data):
     return category
 
 def get_box_size(data):
-    if re.match("^[LS] - +\d{1,2}\.\d lbs\.{0,1}", data.strip()):
+    if re.match("^[LS] - +\d{1,2}(\.\d)? lbs\.?\Z", data.strip()):
         return data.strip()[0]
+    elif data.strip() == '':
+        return 'U'
     else:
         print '[ERROR] Could not import size: ' + data
         global ERROR_COUNT
@@ -35,27 +37,67 @@ def get_box_size(data):
         return 'U'
 
 def get_box_weight(data):
-    if re.match("^[LS] - +\d{1,2}\.\d lbs\.{0,1}", data.strip()):
+    if re.match("^[LS] - +\d{1,2}(\.\d)? lbs\.?", data.strip()):
         return float(data.split('-')[1].strip().split(' ')[0])
+    elif data.strip() == '':
+        return 0.0
     else:
         print '[ERROR] Could not import weight: ' + data
         global ERROR_COUNT
         ERROR_COUNT = ERROR_COUNT + 1
         return 0.0
 
+def add_item(item, box_name_text):
+    try:
+        box_name = BoxName.objects.get(name=box_name_text)
+    except:
+        print '[ERROR] BoxName not found'
+    try:    
+        item = Item.objects.get(name=item, box_name=box_name)
+    except:
+        item = Item(name=item, box_name=box_name)
+        item.save()
+    return item
+
 def get_items(data):
     if re.match("^.*:.*\Z", data.strip()) is not None:
+        global ERROR_COUNT
         box_name_text = data.split(":")[0].strip()
         try:
             box_name = BoxName.objects.get(name=box_name_text)
-            
+
         except:
             print '[ERROR] Could not import items: ' + data
-            global ERROR_COUNT
+            print '[ERROR] With box name ' + box_name_text
             ERROR_COUNT = ERROR_COUNT + 1
             return None
 
-        print box_name
+        items = data[len(box_name_text)+2:].strip()
+        if re.match("^(.*;)*.*\Z", items):
+            item_list = items.split(";")
+            finished_items = []
+            for item in item_list:
+                if re.match('^\d+ [a-z A-Z/&:\'\.]+\Z', item.strip()):
+                    cleaned_item = ' '.join(item.strip().split(' ')[1:])
+                    finished_items.append(add_item(cleaned_item, box_name_text))
+                elif re.match('^[a-z A-Z/&:\'\.\-"]+\Z', item.strip()):
+                    cleaned_item = item.strip()
+                    finished_items.append(add_item(cleaned_item, box_name_text))
+                elif re.match('^[a-z A-Z/&:\'\.\-"]+ [Xx]\d+\Z', item.strip()):
+                    cleaned_item = ' '.join(item.strip().split(' ')[:-1])
+                    finished_items.append(add_item(cleaned_item, box_name_text))
+                elif re.match('^[a-z A-Z/&:\'\.\-"]+ \(\d+\)\Z', item.strip()):
+                    cleaned_item = ' '.join(item.strip().split(' ')[:-1])
+                    finished_items.append(add_item(cleaned_item, box_name_text))
+                else:
+                    print '[ERROR] reading items ' + items
+                    ERROR_COUNT = ERROR_COUNT + 1
+                    return None
+                return finished_items
+        else:
+            print '[ERROR] reading line ' + data
+            ERROR_COUNT = ERROR_COUNT + 1
+            return None
 
 def get_date(data):
     if re.match('^\d{1,2}/\d{4}\Z', data.strip()) is not None:
@@ -66,6 +108,8 @@ def get_date(data):
         return None
     elif re.match('^\Z', data.strip()):
         return None
+    elif data.strip() == 'EXPIRED':
+        return datetime(2000, 1, 1, 0, 0, 0, 0, pytz.UTC)
     else:
         print '[ERROR] Could not import date: ' + data
         global ERROR_COUNT
@@ -94,7 +138,23 @@ def importer(file_name):
 
     print 'Starting new data import'
     for line in csvArray[line_count:]:
-        line_data = [get_category(line[0]), get_box_size(line[1]), get_box_weight(line[1]), get_items(line[2]), get_date(line[3])]
+        line_data = [line[0],get_category(line[0]), get_box_size(line[1]), get_box_weight(line[1]), get_items(line[2]), get_date(line[3])]
+
+        valid = True
+        for data in line_data[:-1]:
+            if data is None:
+                valid = False
+                break
+        if valid:
+            try:
+                box = Box(box_id=line_data[0], box_category=line_data[1], box_size=line_data[2], weight=line_data[3], old_expiration=line_data[5])
+                box.save()
+                for item in line_data[4]:
+                    box_contents = Contents(box_within=box, item=item)
+                    box_contents.save()
+            except:
+                print '[ERROR] save failed'
+
         global READ_LINE_COUNT
         READ_LINE_COUNT = READ_LINE_COUNT + 1
         #print line_data
