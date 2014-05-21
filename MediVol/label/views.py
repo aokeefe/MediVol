da878f6d3929ef220ecbef558692c9b2078616b4
Shun Mok Bhark <shunny@shuns-air.wireless.rit.edu>
2014-05-20 21:22:57 -0400

    from comments

--- MediVol/label/views.py
+++ MediVol/label/views.py
@@ -7,27 +7,18 @@ from django.utils.html import escape
 
 def create_label(request, box_barcode):
 
-  # Get the box information from db 
-  box = Box.objects.get(barcode=box_barcode)
-  box_id = box.box_id
-  box_category_letter = box.box_category.letter
-  box_category_name = box.box_category.name
-  box_contents = Contents.objects.filter(box_within=box) 
-  box_weight = box.weight
-  box_expiration = box.get_expiration_display
+    # Get the box information from db 
+    box = Box.objects.get(barcode=box_barcode)
+    box_contents = Contents.objects.filter(box_within=box) 
 
-  # Get barcode of box 
-  box_barcode = BarcodeGen(box_barcode).get_image()
+    # Get barcode of box 
+    box_barcode = BarcodeGen(box_barcode).get_image()
 
-  # Return the label information
-  context = { 
-      'box_id': box_id,
-      'box_category_letter': box_category_letter,
-      'box_category_name': box_category_name,
+    # Return the label information
+    context = { 
+      'box': box,
       'box_contents': box_contents,
-      'box_weight': box_weight,
-      'box_expiration': box_expiration,
       'box_barcode': box_barcode
-  }
+    }
 
-  return render(request, 'label/label.html', context)
+    return render(request, 'label/label.html', context)
